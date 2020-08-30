9572d679f380a18523bea5d8dd0e7f2ca808d6aa
Michał Górski <mgorski.08@ittcertex.pl>
2020-08-31 00:06:34 +0200

    fix

--- src/main/java/com/example/rectifierBackend/service/RectifierService.java
+++ src/main/java/com/example/rectifierBackend/service/RectifierService.java
@@ -59,11 +59,11 @@ public class RectifierService {
         Process process = processRepository.findById(processId).orElseThrow(() -> new RuntimeException("Process " +
                 "doesn't exist."));
         Thread processThread = new Thread(() -> {
-            System.out.println("In thread");
             BlockingQueue<Event<?>> listener = new LinkedBlockingQueue<>();
             eventService.registerListener(listener);
             Event<?> event = null;
             do {
+                System.out.println("In loop");
                 while(event == null) {
                     try {
                         event = listener.take();
@@ -79,7 +79,6 @@ public class RectifierService {
             } while (!event.getType().equals(Event.PROCESS_STOPPED));
         });
         processThread.start();
-        System.out.println("In controller");
         process.setStartTimestamp(new Timestamp(System.currentTimeMillis()));
         processRepository.save(process);
         eventService.dispatchEvent(new Event<>(Event.PROCESS_STARTED, process));
