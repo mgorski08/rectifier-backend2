4af2f603ee51c8194c3e26ddad22013d0497b279
Michał Górski <mgorski.08@ittcertex.pl>
2020-10-19 13:08:24 +0200

    fix

--- src/main/java/com/example/rectifierBackend/config/WebSecurityConfig.java
+++ src/main/java/com/example/rectifierBackend/config/WebSecurityConfig.java
@@ -17,10 +17,12 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true)
+@EnableTransactionManagement
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     private UserRepository userRepository;
     private JwtAuthEntryPoint unauthorizedHandler;
